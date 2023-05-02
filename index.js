92ddab597087a529ac9a3ea1124f18400beb6cd5
md-toybur-rahman <toyburrahman735@gmail.com>
2023-05-02 11:44:06 +0600

    vercel cofigaration

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 7000;
 
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Awesome Chef is Running");
