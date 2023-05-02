27fa9376e09688a9e08291fb11401161c6bc4b2f
md-toybur-rahman <toyburrahman735@gmail.com>
2023-05-02 20:02:33 +0600

    added single chef data

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 7000;
+const chef = require('./data/chef.json');
 
 const cors = require('cors');
 
 app.use(cors());
 
+// https://awesome-chef-server-md-toybur-rahman.vercel.app/
+
 app.get('/', (req, res) => {
     res.send("Awesome Chef is Running");
 });
+
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chef.find(n => n.id == id);
+    res.send(selectedChef);
+})
 
 app.listen(port, () => {
     console.log(`port no is: ${port}`)
